5153a6d555a8aa4884f9f979ac9955db67063a7a
Allan Borges <allanborges.bsi@gmail.com>
2016-11-22 15:35:52 -0300

    conexao com banco OK

--- dataEduProject/src/br/com/dataeduProject/factory/Conexao.java
+++ dataEduProject/src/br/com/dataeduProject/factory/Conexao.java
@@ -1,5 +1,32 @@
 package br.com.dataeduProject.factory;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 public class Conexao {
+	
+	public static final String USUARIO="smartfier";
+	public static final String SENHA="smartfier";
+	public static final String URL ="jdbc:mysql://nachos.ftp.sh:3306/dataedu";
+	
+	
+	public static Connection conectar()throws SQLException{
+		
+		DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+		Connection conexao = DriverManager.getConnection(URL, USUARIO, SENHA);
+		return conexao;
+	}
+
+	public static void main(String[] args) {
+		try{
+			Connection conexao = Conexao.conectar();	
+			System.out.println("conexao realizada com sucesso");
+		}catch(SQLException e){
+			e.printStackTrace();
+			System.out.println("Nao foi possivel realizar a conexao");
+		}
+		
+		
+	}
 
 }
