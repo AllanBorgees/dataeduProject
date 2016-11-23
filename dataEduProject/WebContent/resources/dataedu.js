5ee6824401f13da646e2f7cd0912af618f1f1108
Allan Borges <allanborges.bsi@gmail.com>
2016-11-23 18:25:25 -0300

    selecionados pronto

--- dataEduProject/WebContent/resources/dataedu.js
+++ dataEduProject/WebContent/resources/dataedu.js
@@ -2,491 +2,422 @@ var app = angular.module('app', [ 'angularjs-dropdown-multiselect' ]);
 
 app.value("urlBase", "http://localhost:8080/dataEduProject/rest/");
 
-app.controller('dataeduController', function($scope, $http, urlBase) {
+app
+		.controller(
+				'dataeduController',
+				function($scope, $http, urlBase) {
 
-	$scope.regioesSelecionada = [];
+					$scope.regioesSelecionada = [];
 
-	$scope.regioes = [ {
-		id : 8,
-		label : "Nordeste"
-	}, {
-		id : 2,
-		label : "Norte"
-	}, {
-		id : 3,
-		label : "Centro-Oeste"
-	}, {
-		id : 4,
-		label : "Sudeste"
-	}, {
-		id : 5,
-		label : "Sul"
-	} ];
+					$scope.regioes = [ {
+						id : 8,
+						label : "Nordeste"
+					}, {
+						id : 2,
+						label : "Norte"
+					}, {
+						id : 3,
+						label : "Centro-Oeste"
+					}, {
+						id : 4,
+						label : "Sudeste"
+					}, {
+						id : 5,
+						label : "Sul"
+					} ];
 
-	$scope.regiaoSettings = {
-		scrollable : true,
-		scrollableHeight : '200px'
-	};
+					$scope.regiaoSettings = {
+						scrollable : true,
+						scrollableHeight : '200px'
+					};
 
-	$scope.selecaoRegiao = []
-	$scope.regioesId = []
-	$scope.submittedRegioes = [];
-	$scope.submitData = function() {
-		$scope.regioesId = [];
-		angular.forEach($scope.regioesSelecionada, function(value, index) {
-			$scope.regioesId.push(value.id);
-			// console.log("-------------------------")
-			// console.log(value.id);
-			// console.log("----------------");
-			// console.log("jjhghg");
-		});
-		// console.log("xxxxxxxxxxxxxxxxxxxxxxxxxxx")
-		// console.log($scope.regioesId);
-		// console.log("!!!!!!!!!!!!!!!!!!!!!!!");
+					$scope.selecaoRegiao = []
+					$scope.regioesId = []
+					$scope.submittedRegioes = [];
 
-		for (var i = 0; i < $scope.regioes.length; i++) {
+					//					
+					//					
+					//					
+					//					
+					//					
+					//					
+					// E S TA DOS
+					//					
+					//					
+					//					
+					//					
+					$scope.estadosSelecionado = [];
 
-			for (var a = 0; a < $scope.regioesId.length; a++) {
-				// console.log("###########");
-				// console.log($scope.regioesId[i]);
-				//								
-				if ($scope.regioes[i].id == $scope.regioesId[a]) {
-					// console.log("@@@@@@@@@@@@@@@@@@@@");
-					// console.log($scope.regioes[i].label);
-					$scope.selecaoRegiao.push($scope.regioes[i].label);
-				}
-			}
-		}
+					$scope.estados = [ {
+						id : 1,
+						label : "Acre-AC"
+					}, {
+						id : 2,
+						label : "Alagoas-AL"
+					}, {
+						id : 3,
+						label : "Amapá-AP"
+					}, {
+						id : 4,
+						label : "Amazonas-AM"
+					}, {
+						id : 5,
+						label : "Bahia-BA"
+					}, {
+						id : 6,
+						label : "Ceará-CE"
+					}, {
+						id : 7,
+						label : "Distrito Federal-DF"
+					}, {
+						id : 8,
+						label : "Espírito Santo-ES"
+					}, {
+						id : 9,
+						label : "Goiás-GO"
+					}, {
+						id : 10,
+						label : "Maranhão-MA"
+					}, {
+						id : 11,
+						label : "Mato Grosso-MT"
+					}, {
+						id : 12,
+						label : "Mato Grosso do Sul-MS"
+					}, {
+						id : 13,
+						label : "Minas Gerais-MG"
+					}, {
+						id : 14,
+						label : "Pará-PA"
+					}, {
+						id : 15,
+						label : "Paraíba-PB"
+					}, {
+						id : 16,
+						label : "Paraná-PR"
+					}, {
+						id : 17,
+						label : "Pernambuco-PE"
+					}, {
+						id : 18,
+						label : "Piauí-PI"
+					}, {
+						id : 19,
+						label : "Rio de Janeiro-RJ"
+					}, {
+						id : 20,
+						label : "Rio Grande do Norte-RN"
+					}, {
+						id : 21,
+						label : "Rio Grande do Sul-RS"
+					}, {
+						id : 22,
+						label : "Rondônia-RO"
+					}, {
+						id : 23,
+						label : "Roraima-RR"
+					}, {
+						id : 24,
+						label : "Santa Catarina-SC"
+					}, {
+						id : 25,
+						label : "São Paulo-SP"
+					}, {
+						id : 26,
+						label : "Sergipe-SE"
+					}, {
+						id : 27,
+						label : "Tocantins-TO"
+					} ];
 
-		console.log($scope.selecaoRegiao);
-	};
+					$scope.estadoSettings = {
+						scrollable : true,
+						scrollableHeight : '200px'
+					};
 
-	//					
-	//					
-	//					
-	//					
-	//					
-	//					
-	// E S TA DOS
-	//					
-	//					
-	//					
-	//					
-	$scope.estadosSelecionado= [];
+					$scope.selecaoEstado = [];
+					$scope.estadosId = [];
+					$scope.submittedEstados = [];
 
-	$scope.estados = [ {
-		id : 1,
-		label : "Acre-AC"
-	}, {
-		id : 2,
-		label : "Alagoas-AL"
-	}, {
-		id : 3,
-		label : "Amapá-AP"
-	}, {
-		id : 4,
-		label : "Amazonas-AM"
-	}, {
-		id : 5,
-		label : "Bahia-BA"
-	}, {
-		id : 6,
-		label : "Ceará-CE"
-	}, {
-		id : 7,
-		label : "Distrito Federal-DF"
-	}, {
-		id : 8,
-		label : "Espírito Santo-ES"
-	}, {
-		id : 9,
-		label : "Goiás-GO"
-	}, {
-		id : 10,
-		label : "Maranhão-MA"
-	}, {
-		id : 11,
-		label : "Mato Grosso-MT"
-	}, {
-		id : 12,
-		label : "Mato Grosso do Sul-MS"
-	}, {
-		id : 13,
-		label : "Minas Gerais-MG"
-	}, {
-		id : 14,
-		label : "Pará-PA"
-	}, {
-		id : 15,
-		label : "Paraíba-PB"
-	}, {
-		id : 16,
-		label : "Paraná-PR"
-	}, {
-		id : 17,
-		label : "Pernambuco-PE"
-	}, {
-		id : 18,
-		label : "Piauí-PI"
-	}, {
-		id : 19,
-		label : "Rio de Janeiro-RJ"
-	}, {
-		id : 20,
-		label : "Rio Grande do Norte-RN"
-	}, {
-		id : 21,
-		label : "Rio Grande do Sul-RS"
-	}, {
-		id : 22,
-		label : "Rondônia-RO"
-	}, {
-		id : 23,
-		label : "Roraima-RR"
-	}, {
-		id : 24,
-		label : "Santa Catarina-SC"
-	}, {
-		id : 25,
-		label : "São Paulo-SP"
-	}, {
-		id : 26,
-		label : "Sergipe-SE"
-	}, {
-		id : 27,
-		label : "Tocantins-TO"
-	} ];
+					//	
+					//	
+					//	
+					//	
+					//	
+					// M U N I C I P I O S
+					//	
+					//	
+					//	
+					//	
+					//	
+					//	
 
+					$scope.municipiosSelecionado = [];
+					$http.get(urlBase + "dataedu/municipios").success(
+							function(data) {
+								console.log(data);
+								$scope.municipios = data.municipio;
 
-	$scope.estadoSettings = {
-		scrollable : true,
-		scrollableHeight : '200px'
-	};
+							}).error(function() {
+						alert('deu certo, mas cade o resultado');
+					});
 
-	$scope.selecaoEstado= [];
-	$scope.estadosId = [];
-	$scope.submittedEstados= [];
-	$scope.submitDataEstado = function() {
+					$scope.municipioSettings = {
+						scrollable : true,
+						scrollableHeight : '200px'
+					};
 
-		angular.forEach($scope.estadosSelecionado, function(value, index) {
-			$scope.estadosId.push(value.id);
-			 console.log("-------------------------")
-			 console.log(value.id);
-			 console.log("----------------");
-			 console.log("jjhghg");
-		});
-		 console.log("xxxxxxxxxxxxxxxxxxxxxxxxxxx")
-		 console.log($scope.estadosId);
-		 console.log("!!!!!!!!!!!!!!!!!!!!!!!");
+					$scope.selecaoMunicipio = [];
+					$scope.municipiosId = [];
+					$scope.submittedMunicipios = [];
+					//
+					//	
+					//	
+					//
+					//	
+					// PORTADOR DE DEFICIENCIA
+					//	
+					//	
+					//	
+					//	
+					//	
+					//	
+					//	
 
-		for (var i = 0; i < $scope.estados.length; i++) {
-			console.log($scope.estados[i].id);
-			for (var a = 0; a < $scope.estadosId.length; a++) {
-										
-				if ($scope.estados[i].id == $scope.estadosId[a]) {
-					$scope.selecaoEstado.push($scope.estados[i].label);
-				}
-			}
-		}
+					$scope.deficientesSelecionado = [];
+					$scope.deficientes = [ {
+						id : 1,
+						label : "Sim"
+					}, {
+						id : 2,
+						label : "Nao"
+					} ];
+					$scope.deficienteSettings = {
+						scrollable : true,
+						scrollableHeight : '200px'
+					};
 
-		console.log($scope.selecaoEstado);
-	};
+					$scope.selecaoDeficiente = [];
+					$scope.deficientesId = [];
+					$scope.submittedDeficientes = [];
+					//	
+					//	
+					//	
+					//	
+					//	
+					// TIPO DE INGRESSO
+					//	
+					//	
+					//	
+					//	
+					//	
 
-	
-//	
-//	
-//	
-//	
-//	
-//		M U N I C I P I O S
-//	
-//	
-//	
-//	
-//	
-//	
-	
-	
-	
-	
-	
-	
-	
-	$scope.municipiosSelecionado = [];
-	$http.get(urlBase + "dataedu/municipios").success(function(data) {
-		console.log(data);
-		$scope.municipios = data.municipio;
+					$scope.tipoDeIngressosSelecionado = [];
+					$scope.tipoDeIngressos = [ {
+						id : 1,
+						label : "Vestibular"
+					}, {
+						id : 2,
+						label : "Enem"
+					}, {
+						id : 3,
+						label : "Avaliação Seriada"
+					}, {
+						id : 4,
+						label : "Seleção Simplificada"
+					}, {
+						id : 5,
+						label : "Seleção Vaga Remanescente"
+					} ];
+					$scope.tipoDeIngressosSettings = {
+						scrollable : true,
+						scrollableHeight : '200px'
+					};
 
-	}).error(function() {
-		alert('deu certo, mas cade o resultado');
-	});
+					$scope.selecaoIngresso = [];
+					$scope.tipoDeIngressosId = [];
+					$scope.submittedtipoDeIngressos = [];
 
-	$scope.municipioSettings = {
-			scrollable : true,
-			scrollableHeight : '200px'
-		};
+					//	
+					//	
+					//	
+					//	
+					//	
+					// APOIO
+					//	
+					//	
+					//	
+					//	
+					//	
+					$scope.apoiosSelecionado = [];
+					$scope.apoios = [ {
+						id : 1,
+						label : "Sim"
+					}, {
+						id : 2,
+						label : "Nao"
+					} ];
+					$scope.apoiosSettings = {
+						scrollable : true,
+						scrollableHeight : '200px'
+					};
 
-		$scope.selecaoMunicipio= [];
-		$scope.municipiosId = [];
-		$scope.submittedMunicipios= [];
-		$scope.submitDataMunicipio = function() {
+					$scope.selecaoApoio = [];
+					$scope.apoiosId = [];
+					$scope.submittedapoios = [];
 
-			angular.forEach($scope.municipiosSelecionado, function(value, index) {
-				$scope.municipiosId.push(value.id);
-				 console.log("-------------------------")
-				 console.log(value.id);
-				 console.log("----------------");
-				 console.log("jjhghg");
-			});
-			 console.log("xxxxxxxxxxxxxxxxxxxxxxxxxxx")
-			 console.log($scope.municipiosId);
-			 console.log("!!!!!!!!!!!!!!!!!!!!!!!");
+					//	
+					//	
+					//	
+					//	
+					//	
+					// BOLSA
+					//	
+					//	
+					//	
+					//	
+					//	
+					//	
 
-			for (var i = 0; i < $scope.municipios.length; i++) {
-				console.log($scope.municipios[i].id);
-				for (var a = 0; a < $scope.municipiosId.length; a++) {
-											
-					if ($scope.municipios[i].id == $scope.municipiosId[a]) {
-						$scope.selecaoMunicipio.push($scope.municipios[i].label);
-					}
-				}
-			}
+					$scope.bolsasSelecionado = [];
+					$scope.bolsas = [ {
+						id : 1,
+						label : "Sim"
+					}, {
+						id : 2,
+						label : "Nao"
+					} ];
+					$scope.bolsasSettings = {
+						scrollable : true,
+						scrollableHeight : '200px'
+					};
 
-			console.log($scope.selecaoMunicipio);
-		};
-//
-//	
-//	
-//
-//	
-//   PORTADOR DE DEFICIENCIA	
-//	
-//	
-//	
-//	
-//	
-//	
-//	
-	
-	
-	$scope.deficientesSelecionado= [];
-	$scope.deficientes = [ {
-		id : 1,
-		label : "Sim"
-	}, {
-		id : 2,
-		label : "Nao"
-	} ];
-	$scope.deficienteSettings = {
-			scrollable : true,
-			scrollableHeight : '200px'
-		};
+					$scope.selecaoBolsa = [];
+					$scope.bolsasId = [];
+					$scope.submittedbolsas = [];
 
-		$scope.selecaoDeficiente= [];
-		$scope.deficientesId = [];
-		$scope.submittedDeficientes= [];
-		$scope.submitDataDeficiente= function() {
+					$scope.submitDataTotal = function() {
+						// /regiao
+						$scope.regioesId = [];
+						angular.forEach($scope.regioesSelecionada, function(
+								value, index) {
+							$scope.regioesId.push(value.id);
 
-			angular.forEach($scope.deficientesSelecionado, function(value, index) {
-				$scope.deficientesId.push(value.id);
-				 console.log("-------------------------")
-				 console.log(value.id);
-				 console.log("----------------");
-				 console.log("jjhghg");
-			});
-			 console.log("xxxxxxxxxxxxxxxxxxxxxxxxxxx")
-			 console.log($scope.deficientesId);
-			 console.log("!!!!!!!!!!!!!!!!!!!!!!!");
+						});
+						for (var i = 0; i < $scope.regioes.length; i++) {
 
-			for (var i = 0; i < $scope.deficientes.length; i++) {
-				console.log($scope.deficientes[i].id);
-				for (var a = 0; a < $scope.deficientesId.length; a++) {
-											
-					if ($scope.deficientes[i].id == $scope.deficientesId[a]) {
-						$scope.selecaoDeficiente.push($scope.deficientes[i].label);
-					}
-				}
-			}
+							for (var a = 0; a < $scope.regioesId.length; a++) {
+								if ($scope.regioes[i].id == $scope.regioesId[a]) {
+									$scope.selecaoRegiao
+											.push($scope.regioes[i].label);
+								}
+							}
+						}
 
-			console.log($scope.selecaoDeficiente);
-		};
-//	
-//	
-//	
-//	
-//	
-//	TIPO DE INGRESSO
-//	
-//	
-//	
-//	
-//	
-	
-	$scope.tipoDeIngressosSelecionado = [];
-	$scope.tipoDeIngressos = [ {
-		id : 1,
-		label : "Vestibular"
-	}, {
-		id : 2,
-		label : "Enem"
-	}, {
-		id : 3,
-		label : "Avaliação Seriada"
-	}, {
-		id : 4,
-		label : "Seleção Simplificada"
-	}, {
-		id : 5,
-		label : "Seleção Vaga Remanescente"
-	} ];
-	$scope.tipoDeIngressosSettings = {
-			scrollable : true,
-			scrollableHeight : '200px'
-		};
+						console.log($scope.selecaoRegiao);
 
-		$scope.selecaoIngresso= [];
-		$scope.tipoDeIngressosId = [];
-		$scope.submittedtipoDeIngressos= [];
-		$scope.submitDatatipoDeIngressos= function() {
+						// /estados
+						angular.forEach($scope.estadosSelecionado, function(
+								value, index) {
+							$scope.estadosId.push(value.id);
+						});
+						for (var i = 0; i < $scope.estados.length; i++) {
+							for (var a = 0; a < $scope.estadosId.length; a++) {
 
-			angular.forEach($scope.tipoDeIngressosSelecionado, function(value, index) {
-				$scope.tipoDeIngressosId.push(value.id);
-				 console.log("-------------------------")
-				 console.log(value.id);
-				 console.log("----------------");
-				 console.log("jjhghg");
-			});
-			 console.log("xxxxxxxxxxxxxxxxxxxxxxxxxxx")
-			 console.log($scope.tipoDeIngressosId);
-			 console.log("!!!!!!!!!!!!!!!!!!!!!!!");
-			 console.log( $scope.tipoDeIngressos);
-			 console.log("#@#@#@$#$%$%#$@#$#%$##$#@$@#$@$#$%$#%$#%$#%%#@#$@$")
-			for (var i = 0; i < $scope.tipoDeIngressos.length; i++) {
-				console.log($scope.tipoDeIngressos[i].id);
-				for (var a = 0; a < $scope.tipoDeIngressosId.length; a++) {
-											
-					if ($scope.tipoDeIngressos[i].id == $scope.tipoDeIngressosId[a]) {
-						$scope.selecaoIngresso.push($scope.tipoDeIngressos[i].label);
-					}
-				}
-			}
+								if ($scope.estados[i].id == $scope.estadosId[a]) {
+									$scope.selecaoEstado
+											.push($scope.estados[i].label);
+								}
+							}
+						}
 
-			console.log($scope.selecaoIngresso);
-		};
+						console.log($scope.selecaoEstado);
 
-	
-//	
-//	
-//	
-//	
-//	
-//	APOIO
-//	
-//	
-//	
-//	
-//	
-	$scope.apoiosSelecionado= [];
-	$scope.apoios = [ {
-		id : 1,
-		label : "Sim"
-	}, {
-		id : 2,
-		label : "Nao"
-	} ];
-	$scope.apoiosSettings = {
-			scrollable : true,
-			scrollableHeight : '200px'
-		};
+						// /municipios
 
-		$scope.selecaoApoio= [];
-		$scope.apoiosId = [];
-		$scope.submittedapoios= [];
-		$scope.submitDataApoio= function() {
+						angular.forEach($scope.municipiosSelecionado, function(
+								value, index) {
+							$scope.municipiosId.push(value.id);
+						});
+						for (var i = 0; i < $scope.municipios.length; i++) {
+							for (var a = 0; a < $scope.municipiosId.length; a++) {
+								if ($scope.municipios[i].id == $scope.municipiosId[a]) {
+									$scope.selecaoMunicipio
+											.push($scope.municipios[i].label);
+								}
+							}
+						}
 
-			angular.forEach($scope.apoiosSelecionado, function(value, index) {
-				$scope.apoiosId.push(value.id);
-				 console.log("-------------------------")
-				 console.log(value.id);
-				 console.log("----------------");
-				 console.log("jjhghg");
-			});
-			 console.log("xxxxxxxxxxxxxxxxxxxxxxxxxxx")
-			 console.log($scope.apoiosId);
-			 console.log("!!!!!!!!!!!!!!!!!!!!!!!");
-			 console.log( $scope.apoios);
-			 console.log("#@#@#@$#$%$%#$@#$#%$##$#@$@#$@$#$%$#%$#%$#%%#@#$@$")
-			for (var i = 0; i < $scope.apoios.length; i++) {
-				console.log($scope.apoios[i].id);
-				for (var a = 0; a < $scope.apoios.length; a++) {
-											
-					if ($scope.apoios[i].id == $scope.apoiosId[a]) {
-						$scope.selecaoApoio.push($scope.apoios[i].label);
-					}
-				}
-			}
+						console.log($scope.selecaoMunicipio);
+						// /deficientes
 
-			console.log($scope.selecaoApoio);
-		};
+						angular.forEach($scope.deficientesSelecionado,
+								function(value, index) {
+									$scope.deficientesId.push(value.id);
+								});
 
-	
-//	
-//	
-//	
-//	
-//	
-//	BOLSA
-//	
-//	
-//	
-//	
-//	
-//	
-	
-	
-	
-	$scope.bolsasSelecionado= [];
-	$scope.bolsas = [ {
-		id : 1,
-		label : "Sim"
-	}, {
-		id : 2,
-		label : "Nao"
-	} ];
-	$scope.bolsasSettings = {
-			scrollable : true,
-			scrollableHeight : '200px'
-		};
+						for (var i = 0; i < $scope.deficientes.length; i++) {
+							for (var a = 0; a < $scope.deficientesId.length; a++) {
 
-		$scope.selecaoBolsa= [];
-		$scope.bolsasId = [];
-		$scope.submittedbolsas= [];
-		$scope.submitDataBolsa= function() {
+								if ($scope.deficientes[i].id == $scope.deficientesId[a]) {
+									$scope.selecaoDeficiente
+											.push($scope.deficientes[i].label);
+								}
+							}
+						}
 
-			angular.forEach($scope.bolsasSelecionado, function(value, index) {
-				$scope.bolsasId.push(value.id);
-				 console.log("-------------------------")
-				 console.log(value.id);
-				 console.log("----------------");
-				 console.log("jjhghg");
-			});
-			 console.log("xxxxxxxxxxxxxxxxxxxxxxxxxxx")
-			 console.log($scope.bolsasId);
-			 console.log("!!!!!!!!!!!!!!!!!!!!!!!");
-			 console.log( $scope.bolsas);
-			 console.log("#@#@#@$#$%$%#$@#$#%$##$#@$@#$@$#$%$#%$#%$#%%#@#$@$")
-			for (var i = 0; i < $scope.bolsas.length; i++) {
-				console.log($scope.bolsas[i].id);
-				for (var a = 0; a < $scope.bolsas.length; a++) {
-											
-					if ($scope.bolsas[i].id == $scope.bolsasId[a]) {
-						$scope.selecaoBolsa.push($scope.bolsas[i].label);
-					}
-				}
-			}
+						console.log($scope.selecaoDeficiente);
+						// /ingressos
 
-			console.log($scope.selecaoBolsa);
-		};
+						angular.forEach($scope.tipoDeIngressosSelecionado,
+								function(value, index) {
+									$scope.tipoDeIngressosId.push(value.id);
+								});
+						for (var i = 0; i < $scope.tipoDeIngressos.length; i++) {
+							for (var a = 0; a < $scope.tipoDeIngressosId.length; a++) {
+								if ($scope.tipoDeIngressos[i].id == $scope.tipoDeIngressosId[a]) {
+									$scope.selecaoIngresso
+											.push($scope.tipoDeIngressos[i].label);
+								}
+							}
+						}
 
+						console.log($scope.selecaoIngresso);
 
-});
+						// /apoio
+
+						angular.forEach($scope.apoiosSelecionado, function(
+								value, index) {
+							$scope.apoiosId.push(value.id);
+						});
+						for (var i = 0; i < $scope.apoios.length; i++) {
+							for (var a = 0; a < $scope.apoios.length; a++) {
+
+								if ($scope.apoios[i].id == $scope.apoiosId[a]) {
+									$scope.selecaoApoio
+											.push($scope.apoios[i].label);
+								}
+							}
+						}
+
+						console.log($scope.selecaoApoio);
+						// /bolsa
+						angular.forEach($scope.bolsasSelecionado, function(
+								value, index) {
+							$scope.bolsasId.push(value.id);
+						});
+						for (var i = 0; i < $scope.bolsas.length; i++) {
+							for (var a = 0; a < $scope.bolsas.length; a++) {
+								if ($scope.bolsas[i].id == $scope.bolsasId[a]) {
+									$scope.selecaoBolsa
+											.push($scope.bolsas[i].label);
+								}
+							}
+						}
+
+						console.log($scope.selecaoBolsa);
+
+					};
+
+				});
